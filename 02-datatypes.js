d6efdca265b6ac96681ead32a26b30465441a3c4
sandhyaPal-26 <palsandhya9591@gmail.com>
2025-11-03 17:25:41 +0000

    datatypes

---
+++ 02-datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all JS code as newer version
+// number => 2 to power 53
+//bigint => for large numbers
+//strings => ""
+//boolean => true/false
+//null => standalone value
+//undefined => (value not assigned)
+//symbol => unique
+
+//object
