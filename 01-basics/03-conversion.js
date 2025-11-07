d47fef8632d3adaeabc1953cc42a530312c8e854
sandhyaPal-26 <palsandhya9591@gmail.com>
2025-11-07 13:55:41 +0000

    conversion of datatypes

---
+++ 01-basics/03-conversion.js
@@ -0,0 +1,29 @@
+let score = true;
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=> NaN
+//true => 1 ; false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "sandhya" => true
+
+let anyNumber = 26
+let stringNumber = String(anyNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+let object = Object(anyNumber)
+console.log(object);
+console.log(typeof object );
