5af96700ef8f9dab9e6b1242320f06fb93cc51a4
sandhyaPal-26 <palsandhya9591@gmail.com>
2025-11-20 11:57:14 +0000

    datesInJS

---
+++ 01-basics/06-dates.js
@@ -0,0 +1,21 @@
+let myDate = new Date()
+console.log(myDate);
+
+//console.log(myDate.toString());
+
+//console.log(myDate.toTimeString());
+
+//console.log(myDate.toLocaleDateString());
+
+console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2025, 8, 26,6,3);
+let myCreatedDate = new Date("09-26-2006");
+console.log(myCreatedDate.toString());
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+
+console.log(Math.floor(Date.now()/1000));
+
